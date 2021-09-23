e5829058d2e6e8ce13fe470d372ac2baea44f154
vjuravlev <vjuravlev@briogroup.ru>
2021-09-23 16:40:07 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -43,8 +43,69 @@
               >
                 <v-icon>mdi-close</v-icon>
               </v-btn>
-              <v-btn color="blue darken-1" text @click="subscribeOnGroup">
+              <v-btn color="#A0847B" text @click="subscribeOnGroup">
                 Подписаться
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="dialogNotifySwitch" scrollable persistent>
+          <v-card color="#F0D5CE">
+            <v-card-title>Ой..</v-card-title>
+            <v-divider></v-divider>
+            <v-card-text
+              style="text-align: center; padding: 20px; font-weight: 500; font-size: 14px;"
+            >
+              <br />
+              Для дальнейшей работы приложения необходимо разрешить доступ к
+              сообществам. Это необходимо для того, чтобы Май смог
+              идентифицировать тебя
+              <br />
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn color="#A0847B" text @click="getUserData">
+                Разрешить
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="dialogVkDonut" scrollable persistent>
+          <v-card color="#FDF5E6">
+            <v-card-title>Упс..</v-card-title>
+            <v-divider></v-divider>
+            <v-card-text
+              style="text-align: center; padding: 20px; font-weight: 500; font-size: 14px;"
+            >
+              Доступ к этому приложению ограничен 😔
+              <br />
+              <br />
+              Это приложение доступно только тем, кто оформил подписку на vk
+              donut в Май.
+              <br />
+              <br />
+              <div style="text-align: left;">
+                <b>В подписку входит:</b>
+                <br />
+                * Консультации с психологами в отдельном чате;<br />
+                * Доступ ко всем приложениям Мая, среди которых: каталог с
+                вопросами на все случаи жизни (Май-аскМи), приложение с тестами,
+                которые помогут лучше узнать себя (Май-тесты) и каталог с
+                упражнениями и практиками (Май-хауТу)<br />
+                * Ранний доступ к новым публикациям Май!
+                <br />
+                <br />
+                Стоимость подписки составляет всего 100 рублей. Если ты ценишь
+                наш труд, подписывайся и пользуйся тем, что мы для тебя создали!
+                Иначе, зачем это все... Мы помогаем тебе, ты поддерживаешь нас.
+              </div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn color="#A0847B" text>
+                Оформить подписку
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -53,12 +114,20 @@
         <v-card>
           <v-tabs-items v-model="tab">
             <v-tab-item>
-              <v-card flat :color="this.colorTheme" :height="this.mainScreenHeight">
+              <v-card
+                flat
+                :color="this.colorTheme"
+                :height="this.mainScreenHeight"
+              >
                 <Main />
               </v-card>
             </v-tab-item>
             <v-tab-item>
-              <v-card flat :color="this.colorTheme" :height="this.mainScreenHeight">
+              <v-card
+                flat
+                :color="this.colorTheme"
+                :height="this.mainScreenHeight"
+              >
                 <Favorites />
               </v-card>
             </v-tab-item>
@@ -104,21 +173,23 @@ import Main from "./components/Main";
 import Favorites from "./components/Favorites";
 
 const group_id = 160404048;
-const token =
-  "f2f21eb0bf879c0e24b9abfeab21fc54b2b3c9a8df571eb5348479ed5ff10857ac0d87e982b79eb1b551e";
 
 export default {
   name: "App",
   components: {
     Main,
-    Favorites
+    Favorites,
   },
   data: () => ({
+    userId: 0,
+
     colorTheme: "#F0D0C7",
     toolbarHeight: 55,
     footerHeight: 110,
     mainScreenHeight: 0,
     dialogSwitch: false,
+    dialogNotifySwitch: false,
+    dialogVkDonut: false,
 
     tab: null,
   }),
@@ -134,6 +205,11 @@ export default {
 
     this.mainScreenHeight =
       screenHeight - this.toolbarHeight - this.footerHeight;
+
+    // bridge
+    //   .send("VKWebAppShowNativeAds", { ad_format: "reward" })
+    //   .then((data) => console.log(data.result))
+    //   .catch((error) => console.log(error));
   },
   methods: {
     subscribeModal() {
@@ -147,30 +223,67 @@ export default {
     getInitialProps() {
       const str = window.location.search.slice(1);
       const objParams = qs.parse(str);
-      let user_id = objParams.vk_user_id;
+      this.userId = objParams.vk_user_id;
       let platform = objParams.vk_platform;
 
       if (platform === "mobile_iphone") {
         this.toolbarHeight = 70;
       }
 
+      this.getUserData();
+    },
+    getUserData() {
       bridge
-        .send("VKWebAppCallAPIMethod", {
-          method: "groups.isMember",
-          request_id: "info",
-          params: {
-            user_id,
-            group_id,
-            v: "5.131",
-            access_token: token,
-          },
+        .send("VKWebAppGetAuthToken", {
+          app_id: 7957050,
+          scope: "groups",
         })
-        .then((res) => {
-          let isMember = res.response;
-          if (!isMember) {
-            setTimeout(() => {
-              this.dialogSwitch = true;
-            }, 12500);
+        .then((r) => {
+          this.dialogNotifySwitch = false;
+
+          let token = r.access_token;
+
+          bridge
+            .send("VKWebAppCallAPIMethod", {
+              method: "groups.isMember",
+              request_id: "info",
+              params: {
+                user_id: this.userId,
+                group_id,
+                v: "5.131",
+                access_token: token,
+              },
+            })
+            .then((res) => {
+              let isMember = res.response;
+              if (!isMember && !this.dialogVkDonut) {
+                setTimeout(() => {
+                  this.dialogSwitch = true;
+                }, 20000);
+              }
+            });
+
+          bridge
+            .send("VKWebAppCallAPIMethod", {
+              method: "donut.isDon",
+              request_id: "info",
+              params: {
+                owner_id: group_id,
+                v: "5.131",
+                access_token: token,
+              },
+            })
+            .then((r) => {
+              if (!r.response) {
+                setTimeout(() => {
+                  this.dialogVkDonut = true;
+                }, 6000);
+              }
+            });
+        })
+        .catch((e) => {
+          if (e.error_data.error_code == 4) {
+            this.dialogNotifySwitch = true;
           }
         });
     },
